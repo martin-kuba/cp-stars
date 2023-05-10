290de556dfa54c7e491814448bc6c1286311ff7b
Luboslav Halama <lubo.halama@gmail.com>
2023-05-10 12:26:32 +0200

    feat(cp-stars): use external AstroSearcher deployed version

--- src/main/java/cz/muni/fi/cpstars/bl/Constants.java
+++ src/main/java/cz/muni/fi/cpstars/bl/Constants.java
@@ -7,8 +7,8 @@ package cz.muni.fi.cpstars.bl;
  */
 public abstract class Constants {
     // Online service used to obtain basic information about astrological objects
-//    public static final String ASTROSEARCHER_BASE_URL = "https://astrosearcher.cerit-sc.cz/";
-    public static final String ASTROSEARCHER_BASE_URL = "http://localhost:8080/";
+    public static final String ASTROSEARCHER_BASE_URL = "https://astrosearcher.cerit-sc.cz/";
+//    public static final String ASTROSEARCHER_BASE_URL = "http://localhost:8080/";
     public static final String ASTROSEARCHER_IDENTIFIERS_JSON_URL = ASTROSEARCHER_BASE_URL + "identifiers/json";
     public static final String ASTROSEARCHER_MEASUREMENTS_JSON_URL = ASTROSEARCHER_BASE_URL + "measurements/json";
     public static final String ASTROSEARCHER_RESULTS_JSON_URL = ASTROSEARCHER_BASE_URL + "results/json";
